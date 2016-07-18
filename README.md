52f66875ada78695fe0dd82cf845aeadc006ef95
rekarnar <rekarnar@gmail.com>
2016-07-18 17:31:21 +0545

    badge updated

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
-# grammar-notes
-
-https://travis-ci.org/rek/grammar-notes.svg?branch=master
+# grammar-notes [![Build Status](https://travis-ci.org/rek/grammar-notes.svg?branch=master)](https://travis-ci.org/rek/grammar-notes)
 
 To help with note taking when writing a grammar.
 
-## To run
+## To run in dev:
 
 ```
     $ npm install
+    $ gulp
+```
+
+## To make a build:
+
+```
+    $ npm install
+    $ gulp build
 ```
--- tasks/sync.js
+++ tasks/sync.js
@@ -1,25 +1,21 @@
-var gulp = require('gulp');
-var newer = require('gulp-newer');
-var merge = require('merge2');
-var paths = require('../paths.js');
-
-// console.log(paths)
+var gulp = require('gulp'),
+	newer = require('gulp-newer'),
+	merge = require('merge2');
 
 module.exports = function() {
 
-	return
+	// Master directory one, we take all files except those
+	// starting with two underscores
+	return gulp.src([paths.srcClient + '/*.html'])
 		// return merge(
-			// Master directory one, we take all files except those
-			// starting with two underscores
-			gulp.src([paths.srcClient + '/*.html'])
-			// check if those files are newer than the same named
-			// files in the destination directory
-				.pipe(newer(paths.distClient))
-			// and if so, copy them
-				.pipe(gulp.dest(paths.distClient))
-			// Slave directory, same procedure here
-			// gulp.src(['./two/**/*', '!./two/**/__*'])
-			// 	.pipe(newer('./one'))
-			// 	.pipe(gulp.dest('./one'))
+		// check if those files are newer than the same named
+		// files in the destination directory
+			.pipe(newer(paths.distClient))
+		// and if so, copy them
+			.pipe(gulp.dest(paths.distClient))
+		// Slave directory, same procedure here
+		// gulp.src(['./two/**/*', '!./two/**/__*'])
+		// 	.pipe(newer('./one'))
+		// 	.pipe(gulp.dest('./one'))
 		// );
 }
