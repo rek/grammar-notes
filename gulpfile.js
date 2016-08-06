8f1152f176824d124020a559ae3c57a85f871918
rekarnar <rekarnar@gmail.com>
2016-08-06 14:04:03 +0545

    wokring on styles

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,22 @@ var gulp = require('gulp'),
 	merge = require('merge-stream'),
 	plugins = require('gulp-load-plugins')();
 
-gulp.task('html', ['clean'], function() {
+gulp.task('html', function() {
 	return gulp.src(pkg.paths.srcClient + '/*.html')
 		.pipe(gulp.dest(pkg.paths.distClient))
 })
 
-gulp.task('js', ['clean'], function() {
+gulp.task('styles', function() {
+	var bootstrap = gulp.src(pkg.paths.bower + '/bootstrap/dist/css/bootstrap.min.css')
+		.pipe(gulp.dest(pkg.paths.distClient + '/styles'))
+
+	var styles = gulp.src(pkg.paths.srcClient + '/styles/*.css')
+		.pipe(gulp.dest(pkg.paths.distClient + '/styles'))
+
+	return merge(bootstrap, styles);
+})
+
+gulp.task('js', function() {
 	var client = gulp.src(pkg.paths.srcClient + '/**/*.js')
 		.pipe(plugins.babel({
 			presets: ['es2015']
@@ -35,10 +45,12 @@ gulp.task('clean', function() {
 		.pipe(gulp.dest(pkg.paths.srcClient))
 });
 
-gulp.task('build', ['js', 'html'], function() {
+gulp.task('build', ['js', 'html', 'styles'], function() {
 });
 
 gulp.task('watch', function() {
+  gulp.watch(pkg.paths.srcClient + '/styles/*.css', ['styles']);
+  gulp.watch(pkg.paths.srcClient + '/*.html', ['html']);
   gulp.watch(pkg.paths.srcClient + '/*.js', ['js']);
   gulp.watch(pkg.paths.srcServer + '/*.js', ['js']);
 });
