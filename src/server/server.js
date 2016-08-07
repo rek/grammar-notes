d33c37770004eb4ed975617f61180f88aeef31d2
rekarnar <rekarnar@gmail.com>
2016-08-07 21:29:08 +0545

    removing old db stuffs

--- src/server/server.js
+++ src/server/server.js
@@ -15,11 +15,8 @@ const port = process.env.PORT || 8080,
 
 let db = null,
 	app = express(),
+	Pool = require('pg').Pool,
     dbDetails = new Object()
-
-let initDb = function initDb(callback) {
-
-}
 
 app.engine('html', require('ejs').renderFile)
 app.use(morgan('combined'))
@@ -33,84 +30,15 @@ app.use('/styles', express.static(__dirname + '/../client/styles'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-	// try to initialize the db on every request if it's not already
-	// initialized.
-	if (!db) {
-		initDb(function (err) {})
-	}
-
-	if (db) {
-		let col = db.collection('counts')
-		// Create a document with request IP and current time of request
-		col.insert({
-			ip: req.ip,
-			date: Date.now()
-		})
-
-		col.count(function (err, count) {
-			res.render('index.html', {
-				pageCountMessage: count,
-				dbInfo: dbDetails
-			})
-		})
-	} else {
-		res.render('index.html', { pageCountMessage: null })
-	}
-})
-
-var Pool = require('pg').Pool;
 // create the pool somewhere globally so its lifetime
 // lasts for as long as your app is running
-var pool = new Pool(process.env.DATABASE_URL)
-// let ddd = Pool.connect(process.env.DATABASE_URL)
+// uses vars from env
+let pool = new Pool()
 
-app.get('/db', function (request, response) {
-	console.log('Connecting to:', process.env.DATABASE_URL);
-
-	pg.connect(process.env.DATABASE_URL, function(err, client) {
-		if (err) throw err;
-
-		console.log('Connected to database!');
-
-		client
-			// .query('SELECT table_schema,table_name FROM information_schema.tables;')
-			.query('SELECT test FROM grammar;')
-			.on('row', function(row) {
-				console.log('Data:', JSON.stringify(row));
-			});
-
-
-	let table = 'test',
-		dbName = 'grammar'
-
-	client.query(`CREATE TABLE IF NOT EXISTS ${dbName}.${table} (
-		title varchar(45) NOT NULL,
-	);`,
-		function(err, rows) {
-			if(err !== null) {
-				console.log(err);
-			}
-			else if(rows === undefined) {
-				console.log('Init time!');
-
-				// db.run('CREATE TABLE "bookmarks" ' +
-				// '("id" INTEGER PRIMARY KEY AUTOINCREMENT, ' +
-				// '"title" VARCHAR(255), ' +
-				// 'url VARCHAR(255))', function(err) {
-				// 	if(err !== null) {
-				// 	console.log(err);
-				// 	}
-				// 	else {
-				// 	console.log("SQL Table 'bookmarks' initialized.");
-				// 	}
-				// });
-			} else {
-				console.log(`SQL Table "${table}" already initialized.`);
-			}
-		});
-	})
-});
+// main app
+app.get('/', function (req, res) {
+	res.render('index.html', { pageCountMessage: null })
+})
 
 /**
 *
@@ -119,9 +47,7 @@ app.get('/db', function (request, response) {
 */
 app.get('/api/test', function(req, res) {
 
-		// Create a document with request IP and current time of request
-		// 	ip: req.ip,
-
+	// Create a log with request IP and current time of request
 	pool.query('INSERT INTO visit (date, ip) VALUES ($1, $2)', [new Date(), req.ip], function(err) {
 		if (err) return handleError(err);
 
@@ -141,25 +67,12 @@ app.use(function(err, req, res, next) {
 	res.status(500).send('Something bad happened!')
 })
 
-// if (!(req.body.firstName || req.body.lastName)) {
-// 	handleError(res, 'Invalid user input', 'Must provide a first or last name.', 400);
-// }
-
-// Generic error handler used by all endpoints.
-// let handleError = (res, reason, message, code) => {
-// 	console.log('ERROR: ' + reason)
-// 	res.status(code || 500).json({'error': message})
-// }
-
 let handleError = (err) => {
 	console.log(e.message, e.stack)
 	res.writeHead(500, {'content-type': 'text/plain'});
+	// res.status(code || 500).json({'error': message})
 	res.end('An error occurred');
 };
-
-initDb(function (err) {
-	console.log('Error connecting to PG. Message:\n' + err)
-})
 
 let runServer = () => {
 	setupDB(pool)
