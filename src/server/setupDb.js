03e07f156d55df7081d072e048826c39011f27c9
rekarnar <rekarnar@gmail.com>
2016-08-07 16:01:28 +0545

    db setup file

---
+++ src/server/setupDb.js
@@ -0,0 +1,16 @@
+
+let setup = (pool) => {
+
+	return pool
+		.query(`CREATE TABLE IF NOT EXISTS visit (
+			date timestamptz,
+			ip varchar(20)
+		)`)
+		.then(() => {
+			return pool.query(`CREATE TABLE IF NOT EXISTS visit3 (
+				date timestamptz
+			)`)
+		})
+}
+
+export default setup
