047f26464e8757af856c2461d461c21396e3b745
rekarnar <rekarnar@gmail.com>
2016-08-13 20:46:27 +0545

    server api GET working

--- src/server/items/endpoints.js
+++ src/server/items/endpoints.js
@@ -1,30 +1,30 @@
 let endpoints = (app, pool, handleError) => {
 
 	app.get('/api/items', function(req, res) {
-		pool.query('SELECT * FROM items', function(err, result) {
+		pool.query('SELECT * FROM item', function(err, result) {
 			// handle an error from the query
 			if (err) {
 				return handleError(err, res)
 			}
 
-			res.writeHead(200, {'content-type': 'application/json'})
-			res.end(result.rows)
+			console.log('Result:', result.rows);
+			// working:
+			// res.setHeader('Content-Type', 'application/json');
+			// res.send(JSON.stringify(result.rows));
+			// working:
+			res.json(result.rows);
+
+			// not working:
+			// res.writeHead(200, {'content-type': 'application/json'})
+			// res.send(JSON.stringify(result.rows)) // JSON string on GET
+			// res.end()
 		})
 	})
 
-	app.get('/api/test', function(req, res) {
-
+	app.post('/api/visit', function(req, res) {
 		// Create a log with request IP and current time of request
 		pool.query('INSERT INTO visit (date, ip) VALUES ($1, $2)', [new Date(), req.ip], function(err) {
-			if (err) return handleError(err)
-
-			// get the total number of visits today (including the current visit)
-			pool.query('SELECT COUNT(date) AS count FROM visit', function(err, result) {
-				// handle an error from the query
-				if (err) return handleError(err)
-				res.writeHead(200, {'content-type': 'application/json'})
-				res.end('You are visitor number ' + result.rows[0].count)
-			})
+			res.json({success: true});
 		})
 	})
 }
--- src/server/server.js
+++ src/server/server.js
@@ -53,22 +53,6 @@ app.get('/health', function (req, res) {
 	res.send('OK')
 })
 
-app.get('/api/test', function(req, res) {
-
-	// Create a log with request IP and current time of request
-	pool.query('INSERT INTO visit (date, ip) VALUES ($1, $2)', [new Date(), req.ip], function(err) {
-		if (err) return handleError(err)
-
-		// get the total number of visits today (including the current visit)
-		pool.query('SELECT COUNT(date) AS count FROM visit', function(err, result) {
-			// handle an error from the query
-			if (err) return handleError(err)
-			res.writeHead(200, {'content-type': 'text/plain'})
-			res.end('You are visitor number ' + result.rows[0].count)
-		})
-	})
-})
-
 // error handling
 app.use(function(error, req, res, next) {
 	console.error(error.stack)
