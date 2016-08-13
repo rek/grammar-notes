59e74c128ff7ad0ce50d336451b781fed78c38ea
rekarnar <rekarnar@gmail.com>
2016-08-13 19:48:00 +0545

    working on db schema

--- src/server/setupDb.js
+++ src/server/setupDb.js
@@ -7,10 +7,53 @@ let setup = (pool) => {
 			ip varchar(20)
 		)`)
 		.then(() => {
-			return pool.query(`CREATE TABLE IF NOT EXISTS visit3 (
+			return pool.query(`CREATE TABLE IF NOT EXISTS item (
+				id bigserial primary key,
+				title varchar(40) NOT NULL,
+				content text,
+				created_at timestamptz,
+				updated_at timestamptz
+			)`)
+		})
+		.then(() => {
+				// id bigserial primary key,
+			return pool.query(`CREATE TABLE IF NOT EXISTS itemType (
+				title varchar(40) NOT NULL UNIQUE,
 				date timestamptz
+			)`)
+		})
+		.then(() => {
+				// id bigserial primary key,
+			return pool.query(`CREATE TABLE IF NOT EXISTS subItem (
+				title varchar(40) NOT NULL,
+				content text,
+				created_at timestamptz,
+				updated_at timestamptz
+			)`)
+		})
+		.then(() => {
+				// id bigserial primary key,
+			return pool.query(`CREATE TABLE IF NOT EXISTS subItemType (
+				title varchar(40) NOT NULL UNIQUE,
+				date timestamptz
+			)`)
+		})
+		.then(() => {
+			return pool.query(`CREATE TABLE IF NOT EXISTS tag (
+				title varchar(40) NOT NULL UNIQUE
+			)`)
+		})
+		.then(() => {
+			return pool.query(`CREATE TABLE IF NOT EXISTS attachment (
+				filename varchar(40) NOT NULL,
+				created_at timestamptz
 			)`)
 		})
 }
 
 export default setup
+
+	title       varchar(40) NOT NULL,
+	did         integer NOT NULL,
+	date_prod   date,
+	kind        varchar(10),
