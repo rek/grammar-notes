216ae6128207273eac23e05f389d57fbaf6bf06c
rekarnar <rekarnar@gmail.com>
2016-08-17 20:10:28 +0545

    adding item handlers

---
+++ src/server/item/handlers.js
@@ -0,0 +1,56 @@
+import _ from 'lodash'
+import {inserter} from '../utils'
+
+let endpoints = (app, pool, handleError) => {
+
+	app.get('/api/items', function(req, res) {
+		pool.query('SELECT * FROM item', function(err, result) {
+			// handle an error from the query
+			if (err) {
+				return handleError(err, res)
+			}
+
+			console.log('Result:', result.rows);
+			// working:
+			// res.setHeader('Content-Type', 'application/json');
+			// res.send(JSON.stringify(result.rows));
+			// working:
+			res.json(result.rows);
+
+			// not working:
+			// res.writeHead(200, {'content-type': 'application/json'})
+			// res.send(JSON.stringify(result.rows)) // JSON string on GET
+			// res.end()
+		})
+	})
+
+	app.post('/api/items', function(req, res) {
+		let validItems = {}
+
+		// console.log('Raw:', req.body);
+
+		if (_.isString(req.body.item_title) && req.body.item_title !== '') {
+
+			validItems.item_title = req.body.item_title
+		}
+
+		if (_.isString(req.body.content) && req.body.content !== '') {
+			validItems.content = req.body.content
+		}
+
+		let sql = inserter(table, validItems)
+
+		pool.query(sql.query, sql.data, (err) => {
+			res.json({success: true});
+		})
+	})
+
+	// app.post('/api/visit', function(req, res) {
+	// 	// Create a log with request IP and current time of request
+	// 	pool.query('INSERT INTO visit (date, ip) VALUES ($1, $2)', [new Date(), req.ip], function(err) {
+	// 		res.json({success: true});
+	// 	})
+	// })
+}
+
+export default endpoints
