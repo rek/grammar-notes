fe58cc8a83f9b962a8cbc2e0ff74a843b6ee6423
rekarnar <rekarnar@gmail.com>
2016-08-20 20:39:04 +0545

    adding new files

---
+++ src/client/scripts/app/router.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Router, Route, browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
+import ItemComponents from '../items/components';
+// import TestPage from './TestPage';
+
+export default class AppRouter extends React.Component {
+    constructor() {
+        super();
+            // <Route path="test" component={TestPage}/>
+        this.routes = (<Route path="/" component={ItemComponents}>
+        </Route>);
+    }
+
+    componentWillMount() {
+        this.forceUpdate(); // a little hack to help us re-render when this module is reloaded
+    }
+
+    render() {
+        return (
+            <Provider store={this.props.store}>
+                <Router history={browserHistory}>{this.routes}</Router>
+            </Provider>
+        );
+    }
+}
+
+AppRouter.propTypes = {
+    store: React.PropTypes.object
+};
