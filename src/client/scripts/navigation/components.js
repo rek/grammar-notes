b9625bfd0e35751317de519472751e9a6b5f511f
rekarnar <rekarnar@gmail.com>
2016-09-04 17:47:47 +0545

    adding nav

---
+++ src/client/scripts/navigation/components.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import {Navbar, Nav, NavItem} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
+import {connect} from 'react-redux'
+
+import notiActions from '../notifications/actions'
+import Notification from '../notifications/components'
+
+export class App extends React.Component {
+
+	componentWillMount() {
+		this.context.router.listen(() => {
+			this.props.dismissNotification()
+		})
+	}
+
+	render() {
+		const {alertType, message, dismissNotification} = this.props
+		return (
+			<div>
+				<Navbar>
+					<Navbar.Header>
+						<Navbar.Brand>
+							<LinkContainer to="/">
+								<a href="/">Grammar notes</a>
+							</LinkContainer>
+						</Navbar.Brand>
+					</Navbar.Header>
+					<Nav>
+						<LinkContainer to="/test">
+							<NavItem>Test</NavItem>
+						</LinkContainer>
+					</Nav>
+				</Navbar>
+				<Notification alertType={alertType} message={message} onDismiss={dismissNotification}/>
+				{this.props.children}
+			</div>
+		)
+	}
+}
+
+App.contextTypes = {
+	router: React.PropTypes.object.isRequired
+}
+
+App.propTypes = {
+	dismissNotification: React.PropTypes.func,
+	message: React.PropTypes.string,
+	alertType: React.PropTypes.string,
+	children: React.PropTypes.object
+}
+
+export default connect((state) => state.AppReducer, notiActions)(App)
