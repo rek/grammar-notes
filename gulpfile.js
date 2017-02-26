03f5301e5ac6f8be7f98aac017802a50ccc02cda
rekarnar <rekarnar@gmail.com>
2017-02-26 18:13:24 +0545

    upgraded to gulp 4 config

--- gulpfile.js
+++ gulpfile.js
@@ -74,11 +74,6 @@ gulp.task('js', function() {
 	return merge(client);
 })
 
-gulp.task('test', ['build'], function() {
-	return gulp.src('test/**/test/*.js')
-		.pipe(plugins.mocha({reporter: 'nyan'}))
-})
-
 gulp.task('clean', function() {
 	let styles = gulp.src('styles/bootstrap*.css')
 		.pipe(plugins.clean({force: true}))
@@ -92,35 +87,34 @@ gulp.task('clean', function() {
 });
 
 gulp.task('watch', function() {
-	gulp.watch('styles/*.css', ['styles']);
-	gulp.watch('index.dev.html', ['html-dev']);
+	gulp.watch('styles/*.css', gulp.series('styles'));
+	gulp.watch('index.dev.html', gulp.series('html-dev'));
 });
 
-gulp.task('build', ['html-dev', 'styles'], function() {
-});
+gulp.task('build', gulp.series('html-dev', 'styles'))
 
-gulp.task('build-prod', ['html-prod', 'styles'], function() {
-});
+gulp.task('build-prod', gulp.series('html-prod', 'styles'))
+
+gulp.task('server', function(callback) {
+	exec('node server.js', function(err, stdout, stderr) {
+		console.log(stdout)
+		console.log(stderr)
+		callback(err)
+	})
+})
 
 gulp.task('dev',
 	gulp.series('build', gulp.parallel('watch', 'server'))
 )
 
-// gulp.task('dev', function() {
-// 	gulp.parallel(['build', 'watch', 'server'])
-// })
-
-gulp.task('server', function(callback) {
-	exec('node server.js', function(err, stdout, stderr) {
-		console.log(stdout);
-		console.log(stderr);
-		callback(err);
-	});
-})
-
 // var spawn = require('child_process').spawn;
-gulp.task('default', ['build', 'watch'], function(callback) {
+gulp.task('default', gulp.series('build', 'watch'), function(callback) {
 	// spawn('node', ['devServer.js'], {stdio: 'inherit'});
 
 	callback()
 });
+
+gulp.task('test', gulp.series('build'), function() {
+	return gulp.src('test/**/test/*.js')
+		.pipe(plugins.mocha({reporter: 'nyan'}))
+})
