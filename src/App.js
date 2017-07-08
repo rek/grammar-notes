a93d1c8c138b50991c83e5d5f34d855b35029eda
Adam Tombleson <rekarnar@gmail.com>
2017-07-08 21:17:08 +0545

    setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux'
+import {Provider} from 'react-redux';
+
 import logo from "./logo.svg";
 import "./App.css";
+import "./styles/bootstrap.css";
+import "./styles/bootstrap-theme.css";
 
 import {
   BrowserRouter as Router,
@@ -12,6 +17,8 @@ import ItemList from "./items/components/list";
 import ItemCreate from "./items/components/create";
 import ItemEdit from "./items/components/edit";
 import ItemShow from "./items/components/show";
+
+import Nav from "./navigation/components/main";
 
 const Home = () =>
   <div>
@@ -30,18 +37,22 @@ const Menu = () =>
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to Grammar Notes</h2>
-          </div>
+      <Provider store={this.props.store}>
+        <Router store={this.props.store}>
+          <div className="App">
+            <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Welcome to Grammar Notes</h2>
+            </div>
 
-          <Menu />
-        </div>
-      </Router>
+            <Nav />
+            <Menu />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
 
 export default App;
+// export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import {combineReducers, createStore, applyMiddleware, compose} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import NotificationReducer from './notifications/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let devTools = window.devToolsExtension ?
+	window.devToolsExtension() :
+	(f) => {
+		console.log('What is f', f);
+		return f
+	}
+
+let store = createStore(combineReducers({
+    // AppReducer,
+    NotificationReducer,
+    // ItemsReducer
+}),
+// default state:
+{},
+//
+compose(applyMiddleware(thunkMiddleware), devTools));
+
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 registerServiceWorker();
